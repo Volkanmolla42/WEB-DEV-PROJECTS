ac30e7289c4b94e57f4cf8bdc5f3c40f4487a91f
Volkan Molla <mollavolkan11@gmail.com>
2024-08-31 19:59:18 +0300

    Learn promises and convert code to promises

--- SSD-JS/Amazon/data/cart.js
+++ SSD-JS/Amazon/data/cart.js
@@ -53,3 +53,15 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 export function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart))
 }
+
+export function loadCart(fun) {
+    const xhr = new XMLHttpRequest()
+
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response)
+        fun()
+    })
+
+    xhr.open('GET', 'https://supersimplebackend.dev/cart')
+    xhr.send()
+}
