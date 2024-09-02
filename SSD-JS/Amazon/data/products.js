b075a0fac06832a19367c38c48aa2acf32355d4a
Volkan Molla <mollavolkan11@gmail.com>
2024-09-02 15:09:12 +0300

    convert loadProducts from xhr to fetch

--- SSD-JS/Amazon/data/products.js
+++ SSD-JS/Amazon/data/products.js
@@ -1,103 +1,127 @@
 import formatCurrency from '../scripts/utils/money.js'
 
 export function getProduct(productId) {
-    let matchingProduct
-    products.forEach((product) => {
-        if (product.id === productId) matchingProduct = product
-    })
-    return matchingProduct
+  let matchingProduct
+  products.forEach((product) => {
+    if (product.id === productId) matchingProduct = product
+  })
+  return matchingProduct
 }
 
 export class Product {
-    id
-    image
-    name
-    rating
-    priceCents
-    constructor(productDetails) {
-        this.id = productDetails.id
-        this.image = productDetails.image
-        this.name = productDetails.name
-        this.rating = productDetails.rating
-        this.priceCents = productDetails.priceCents
-    }
+  id
+  image
+  name
+  rating
+  priceCents
+  constructor(productDetails) {
+    this.id = productDetails.id
+    this.image = productDetails.image
+    this.name = productDetails.name
+    this.rating = productDetails.rating
+    this.priceCents = productDetails.priceCents
+  }
 
-    //! daha kısa hali
-    /* constructor({ id, image, name, rating, priceCents }) {
+  //! daha kısa hali
+  /* constructor({ id, image, name, rating, priceCents }) {
     Object.assign(this, { id, image, name, rating, priceCents });
   } */
 
-    getStarsUrl() {
-        return `images/ratings/rating-${this.rating.stars * 10}.png`
-    }
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
 
-    getPrice() {
-        return `$${formatCurrency(this.priceCents)}`
-    }
-    extraInfoHTML() {
-        return ``
-    }
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
+  extraInfoHTML() {
+    return ``
+  }
 }
 
 export class Clothing extends Product {
-    sizeChartLink
+  sizeChartLink
 
-    constructor(productDetails) {
-        super(productDetails)
-        this.sizeChartLink = productDetails.sizeChartLink
-    }
-    extraInfoHTML() {
-        //super.extraInfoHTML() //! parent fonksiyonu çağırmak için yazılır
+  constructor(productDetails) {
+    super(productDetails)
+    this.sizeChartLink = productDetails.sizeChartLink
+  }
+  extraInfoHTML() {
+    //super.extraInfoHTML() //! parent fonksiyonu çağırmak için yazılır
 
-        return `
+    return `
         <a href="${this.sizeChartLink}" target="_blank">
             Size chart
         </a>
         `
-    }
+  }
 }
 
 export class Appliance extends Product {
-    instructionsLink
-    warrantyLink
+  instructionsLink
+  warrantyLink
 
-    constructor(productDetails) {
-        super(productDetails)
-        this.instructionsLink = productDetails.instructionsLink
-        this.warrantyLink = productDetails.warrantyLink
-    }
-    extraInfoHTML() {
-        return `
+  constructor(productDetails) {
+    super(productDetails)
+    this.instructionsLink = productDetails.instructionsLink
+    this.warrantyLink = productDetails.warrantyLink
+  }
+  extraInfoHTML() {
+    return `
          <a href="${this.instructionsLink}" target="_blank">
             Instructions
         </a> <a href="${this.warrantyLink}" target="_blank">
             Warranty
         </a>
         `
-    }
+  }
 }
 
 export let products = []
-
-export function loadProducts(fun) {
-    const xhr = new XMLHttpRequest()
-
-    xhr.addEventListener('load', () => {
-        products = JSON.parse(xhr.response).map((productDetails) => {
-            if (productDetails.type === 'clothing') {
-                return new Clothing(productDetails)
-            } else if (productDetails.type === 'appliance') {
-                return new Appliance(productDetails)
-            } else {
-                return new Product(productDetails)
-            }
-        })
-        console.log('load products')
-        fun()
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products')
+    .then((response) => {
+      return response.json()
     })
+    .then((productsData) => {
+      console.log(productsData)
 
-    xhr.open('GET', 'https://supersimplebackend.dev/products')
-    xhr.send()
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === 'clothing') {
+          return new Clothing(productDetails)
+        } else if (productDetails.type === 'appliance') {
+          return new Appliance(productDetails)
+        } else {
+          return new Product(productDetails)
+        }
+      })
+      console.log('load products')
+    })
+  return promise
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step')
+})*/
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails)
+      } else if (productDetails.type === 'appliance') {
+        return new Appliance(productDetails)
+      } else {
+        return new Product(productDetails)
+      }
+    })
+    console.log('load products')
+    fun()
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products')
+  xhr.send()
 }
 
 /*
--- SSD-JS/Amazon/tests/checkout/orderSummaryTest.js
+++ SSD-JS/Amazon/tests/checkout/orderSummaryTest.js
@@ -1,89 +1,87 @@
 import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js'
 import { cart } from '../../data/cart-class.js'
-import { loadProducts } from '../../data/products.js'
+import { loadProducts, loadProductsFetch } from '../../data/products.js'
 
 describe('test suite: renderOrderSummary', () => {
-    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
-    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
-    beforeAll((done) => {
-        loadProducts(() => {
-            done()
-        })
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+  beforeAll((done) => {
+    loadProductsFetch().then(() => {
+      done()
     })
-    beforeEach(() => {
-        spyOn(localStorage, 'setItem')
-        document.querySelector('.js-test-container').innerHTML = `,
+  })
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem')
+    document.querySelector('.js-test-container').innerHTML = `,
           <div class="js-order-summary"></div>
           <div class="js-payment-summary"></div>
           <div class="js-checkout-header"></div>
 
     `
 
-        spyOn(localStorage, 'getItem').and.callFake(() => {
-            return JSON.stringify([
-                {
-                    productId: productId1,
-                    quantity: 2,
-                    deliveryOptionId: '1',
-                },
-                {
-                    productId: productId2,
-                    quantity: 1,
-                    deliveryOptionId: '1',
-                },
-            ])
-        })
-        cart.loadFromStorage()
-        renderOrderSummary()
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: '1',
+        },
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: '1',
+        },
+      ])
     })
-    afterEach(() => {
-        document.querySelector('.js-test-container').innerHTML = ``
-    })
-    it('displays the cart', () => {
-        expect(
-            document.querySelectorAll('.js-cart-item-container').length,
-        ).toEqual(2)
-        expect(
-            document.querySelector(`.js-product-quantity-${productId1}`)
-                .innerText,
-        ).toContain('Quantity: 2')
-        expect(
-            document.querySelector(`.js-product-quantity-${productId2}`)
-                .innerText,
-        ).toContain('Quantity: 1')
-        expect(
-            document.querySelector(`.js-product-name-${productId1}`).innerText,
-        ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs')
-        expect(
-            document.querySelector(`.js-product-name-${productId2}`).innerText,
-        ).toEqual('Intermediate Size Basketball')
-        expect(
-            document.querySelector(`.js-product-price-${productId1}`).innerText,
-        ).toEqual('$10.90')
-        expect(
-            document.querySelector(`.js-product-price-${productId2}`).innerText,
-        ).toEqual('$20.95')
-    })
-    it('removes a product', () => {
-        document.querySelector(`.js-delete-link-${productId1}`).click()
-        expect(
-            document.querySelectorAll('.js-cart-item-container').length,
-        ).toEqual(1)
-        expect(
-            document.querySelector(`.js-cart-item-container-${productId1}`),
-        ).toEqual(null)
+    cart.loadFromStorage()
+    renderOrderSummary()
+  })
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = ``
+  })
+  it('displays the cart', () => {
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(
+      2,
+    )
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText,
+    ).toContain('Quantity: 2')
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText,
+    ).toContain('Quantity: 1')
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText,
+    ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs')
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText,
+    ).toEqual('Intermediate Size Basketball')
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText,
+    ).toEqual('$10.90')
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText,
+    ).toEqual('$20.95')
+  })
+  it('removes a product', () => {
+    document.querySelector(`.js-delete-link-${productId1}`).click()
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(
+      1,
+    )
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId1}`),
+    ).toEqual(null)
 
-        expect(
-            document.querySelector(`.js-cart-item-container-${productId2}`),
-        ).not.toEqual(null)
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId2}`),
+    ).not.toEqual(null)
 
-        expect(cart.cartItems.length).toEqual(1)
-        expect(cart.cartItems[0].productId).toEqual(productId2)
-        expect(
-            document.querySelector(`.js-product-name-${productId2}`).innerText,
-        ).toEqual('Intermediate Size Basketball')
-        expect(
-            document.querySelector(`.js-product-price-${productId2}`).innerText,
-        ).toEqual('$20.95')
-    })
+    expect(cart.cartItems.length).toEqual(1)
+    expect(cart.cartItems[0].productId).toEqual(productId2)
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText,
+    ).toEqual('Intermediate Size Basketball')
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText,
+    ).toEqual('$20.95')
+  })
 })
